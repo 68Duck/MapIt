e0a892cf91ac837b0a5d751b3bdd0827cbeaba13
68Duck <josh@68duck.co.uk>
2025-06-01 15:04:17 +0100

    Style: Fixed some minor style issues

--- MapIt/R/add_pie_charts.R
+++ MapIt/R/add_pie_charts.R
@@ -16,27 +16,28 @@ library(ggnewscale)
 #'   fill the pie charts.
 #' @param legend_title A string to be used as the title of the legend.
 #' @param pie_scale The size of the pie charts
-#' 
+#'
 #' @return A `ggplot` object containing the pie layer
-#' 
+#'
 #' @examples
 #' data <- data.frame(x = c(1, 2, 3), y = c(4, 5, 6),
 #'  A = c(30, 50, 20), B = c(70, 50, 80))
 #' map <- map + add_pie_charts(data, x = "x", y = "y",
 #'  attributes = c("A", "B"), legend_title = "Category", pie_scale = 0.1)
-#' 
+#'
 #' @import ggplot2
 #' @import scatterpie
 #' @import ggnewscale
 #' @export
-add_pie_charts <- function(data, x, y, attributes, legend_title = NULL, pie_scale = 1) {
+add_pie_charts <- function(data, x, y, attributes,
+                           legend_title = NULL, pie_scale = 1) {
   layers <- list(
-    new_scale("fill"),
-    geom_scatterpie(aes_string(x = x, y = y),
-                     data = as.data.frame(data),
-                     cols = attributes,
-                     pie_scale = pie_scale),
-    coord_sf())
+                 new_scale("fill"),
+                 geom_scatterpie(aes_string(x = x, y = y),
+                                 data = as.data.frame(data),
+                                 cols = attributes,
+                                 pie_scale = pie_scale),
+                 coord_sf())
   if (!is.null(legend_title)) {
     layers <- c(layers, labs(fill = legend_title))
   }
--- MapIt/R/choropleth.R
+++ MapIt/R/choropleth.R
@@ -3,30 +3,31 @@ library(rnaturalearth)
 library(scales)
 
 #' Create a Choropleth Map
-#' 
+#'
 #' Generates a choropleth map using ggplot2
-#' 
-#' @param data A `sf` (simple features) object containing the data to be plotted.
+#'
+#' @param data A `sf` (simple features) object containing the
+#' data to be plotted.
 #' @param fill The variable in `data` to be used for filling the regions.
 #' @param legend_title The title of the legend.
-#' 
+#'
 #' @return A `ggplot` object representing the choropleth map.
-#' 
+#'
 #' @examples
 #' choropleth(sf_data, fill = "population_density",
 #'  legend_title = "Population Density")
-#' 
+#'
 #' @import ggplot2
 #' @import sf
 #' @import scales
 #' @export
 choropleth <- function(data, fill, legend_title = NULL) {
-    graph <- ggplot() + geom_sf(data=data, aes(fill = {{fill}})) +
-        theme_void() +
-        scale_color_gradient2(low = "red", high = "blue",
-        mid = "white", midpoint = 0)
-    if (!is.null(legend_title)) {
-        graph$labels$fill <- legend_title
-    }
-    graph
+  graph <- ggplot() + geom_sf(data = data, aes(fill = {{fill}})) +
+    theme_void() +
+    scale_color_gradient2(low = "red", high = "blue",
+                          mid = "white", midpoint = 0)
+  if (!is.null(legend_title)) {
+    graph$labels$fill <- legend_title
+  }
+  graph
 }
